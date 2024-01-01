95818c5b7c13774433286b74a2a7080c328db710
Slash Hj Tapah <imuqri31@gmail.com>
2024-01-02 05:00:25 +0800

    Update Header.jsx
    add account and logout dropdown

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,12 +1,40 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useState } from "react";
+import { Button, Dropdown, Menu } from "antd";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UserOutlined,
+  FileTextOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 
 const Header = ({ collapsed, setCollapsed }) => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleMenuClick = (e) => {
+    if (e.key === "logout") {
+      // Add logout functionality here
+      console.log("Logout clicked");
+    }
+    // Add additional menu options and handling as needed
+    setDropdownVisible(false); // Hide the dropdown after clicking an option
+  };
+
+  const accountMenu = (
+    <Menu onClick={handleMenuClick} style={{ minWidth: "150px" }}>
+      <Menu.Item key="account" icon={<UserOutlined />}>
+        My Account
+      </Menu.Item>
+      <Menu.Item key="myreport" icon={<FileTextOutlined />}>
+        My Report
+      </Menu.Item>
+      <Menu.Item key="logout" icon={<LogoutOutlined />}>
+        Logout
+      </Menu.Item>
+      {/* Add more menu items as needed */}
+    </Menu>
+  );
+
   return (
     <div
       style={{
@@ -29,19 +57,23 @@ const Header = ({ collapsed, setCollapsed }) => {
         }}
       />
 
-      <Button
-        type="text"
-        icon={<UserOutlined />}
-        onClick={() => {
-          console.log("Account button clicked");
-        }}
-        style={{
-          fontSize: "16px",
-          width: 64,
-          height: 64,
-          background: "#FFFFFF",
-        }}
-      />
+      <Dropdown
+        overlay={accountMenu}
+        placement="bottomRight"
+        visible={dropdownVisible}
+        onVisibleChange={(visible) => setDropdownVisible(visible)}
+      >
+        <Button
+          type="text"
+          icon={<UserOutlined />}
+          style={{
+            fontSize: "16px",
+            width: 64,
+            height: 64,
+            background: "#FFFFFF",
+          }}
+        />
+      </Dropdown>
     </div>
   );
 };
